3d6390c6e05a0a0f987fa29c51b93156176224aa
Your Name <you@example.com>
2023-11-29 16:52:46 -0500

    first commit

---
+++ src/main/java/com/lizana/msoperacion/MsOperacionApplication.java
@@ -0,0 +1,13 @@
+package com.lizana.msoperacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOperacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOperacionApplication.class, args);
+	}
+
+}
